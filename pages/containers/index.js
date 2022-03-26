b2349822fd3b6a879f632202f91962296ca0a534
minhquyn1204 <68966267+minhquyn1204@users.noreply.github.com>
2022-03-26 15:54:02 +0700

    update phan-khu

--- pages/containers/index.js
+++ pages/containers/index.js
@@ -9,7 +9,7 @@ import Ground from "../mat-bang";
 import Position from "../vi-tri";
 import Ecological from "../sinh-thai";
 import Utilities from "../tien-ich";
-
+import Sliders from "../phan-khu";
 import styles from "./styles.module.scss";
 import NavBars from "../../components/NavBars";
 import clsx from "clsx";
@@ -20,52 +20,46 @@ export default function Containers() {
 
   const [timeStamp, settimeStamp] = useState(0);
 
-
   const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  
-  const handleOnWheel = (event) => {
-      if(Number(event.nativeEvent.timeStamp) - timeStamp > 1000){
-        if(event.nativeEvent.deltaY > 0) {
-          setActive(prev => {
-            if(prev < 9) {
-              return prev + 1
-            } else{
-              return 9
-            }
-          })
-        } else {
-          setActive(prev => {
-            if(prev > 0) {
-              return prev - 1
-            } else {
-              return 0
-            }
-          })
-        }
-        settimeStamp(event.nativeEvent.timeStamp)
-      }
-          
-  }
 
- 
+  const handleOnWheel = (event) => {
+    if (Number(event.nativeEvent.timeStamp) - timeStamp > 1000) {
+      if (event.nativeEvent.deltaY > 0) {
+        setActive((prev) => {
+          if (prev < 9) {
+            return prev + 1;
+          } else {
+            return 9;
+          }
+        });
+      } else {
+        setActive((prev) => {
+          if (prev > 0) {
+            return prev - 1;
+          } else {
+            return 0;
+          }
+        });
+      }
+      settimeStamp(event.nativeEvent.timeStamp);
+    }
+  };
+
   return (
-    <div className={styles.containers} 
-      onWheel={handleOnWheel}
-    >
-      
-      <NavBars 
+    <div className={styles.containers} onWheel={handleOnWheel}>
+      <NavBars
         setPrevIndex={setPrevIndex}
         setActive={setActive}
         active={active}
       />
       {nums.map((item, index) => (
-        <div 
+        <div
           key={index}
           className={clsx(styles.slide, {
             [styles.active]: Number(active) === index,
             [styles.current]: Number(prevIndex) === index,
-            [styles.beforeActive] : Number(active) > index,
-            [styles.afterActive] : Number(active) < index,
+            [styles.beforeActive]: Number(active) > index,
+            [styles.afterActive]: Number(active) < index,
           })}
         >
           {index === 0 && <HomePage />}
@@ -73,8 +67,8 @@ export default function Containers() {
           {index === 2 && <Position />}
           {index === 3 && <Ecological />}
           {index === 4 && <Utilities />}
-          {index === 5 && <HomePage />}
-          {index === 6 && <Ground />}
+          {index === 5 && <Sliders />}
+          {index === 6 && <HomePage />}
           {index === 7 && <Position />}
           {index === 8 && <Ecological />}
           {index === 9 && <Utilities />}
