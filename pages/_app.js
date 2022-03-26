2f3539223ffc87901e58a2228b4eff20cb88f134
minhquyn1204 <68966267+minhquyn1204@users.noreply.github.com>
2022-03-26 14:37:12 +0700

    slider aqua6

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import Layout from '../components/Layout'
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Layout>
-          <Component {...pageProps} />
+        <Component {...pageProps} />
       </Layout>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/phan-khu/Slider.jsx
@@ -0,0 +1,153 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import styles from "./slider.module.scss";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+import Link from "next/link";
+
+export default function Sliders() {
+  return (
+    <div className={styles.slider}>
+      <div className={styles.ar_content}>
+        <Swiper
+          style={{ maxWidth: "960px" }}
+          centeredSlides={true}
+          slidesPerView={1.1}
+          speed={1200}
+          loop={true}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className={styles.item}>
+              <Link href="/">
+                <img
+                  src="https://aquacity.com.vn/wp-content/uploads/2021/12/Background-trainghiemtour-01-1.jpg"
+                  alt=""
+                />
+              </Link>
+              <div className={styles.content}>
+                <div className={styles.desc_box}>
+                  <div className={styles.row_content}>
+                    <div className={styles.col_content}>
+                      <h2>Hành trình tham quan</h2>
+                    </div>
+                    <div className={styles.col_contents}>
+                      <p>điểm đến phồn hoa</p>
+                      <p>Sinh thái giao hoà</p>
+                    </div>
+                  </div>
+                </div>
+                <span>khám phá ngay</span>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.item}>
+              <Link href="/">
+                <img
+                  src="https://aquacity.com.vn/wp-content/uploads/2021/12/Background-trainghiemtour-01-1.jpg"
+                  alt=""
+                />
+              </Link>
+              <div className={styles.content}>
+                <div className={styles.desc_box}>
+                  <div className={styles.row_content}>
+                    <div className={styles.col_content}>
+                      <h2>Hành trình tham quan</h2>
+                    </div>
+                    <div className={styles.col_contents}>
+                      <p>điểm đến phồn hoa</p>
+                      <p>Sinh thái giao hoà</p>
+                    </div>
+                  </div>
+                </div>
+                <span>khám phá ngay</span>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.item}>
+              <Link href="/">
+                <img
+                  src="https://aquacity.com.vn/wp-content/uploads/2021/12/Background-trainghiemtour-01-1.jpg"
+                  alt=""
+                />
+              </Link>
+              <div className={styles.content}>
+                <div className={styles.desc_box}>
+                  <div className={styles.row_content}>
+                    <div className={styles.col_content}>
+                      <h2>Hành trình tham quan</h2>
+                    </div>
+                    <div className={styles.col_contents}>
+                      <p>điểm đến phồn hoa</p>
+                      <p>Sinh thái giao hoà</p>
+                    </div>
+                  </div>
+                </div>
+                <span>khám phá ngay</span>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.item}>
+              <Link href="/">
+                <img
+                  src="https://aquacity.com.vn/wp-content/uploads/2021/04/dao-phuong-hoang-thumbnail.jpg"
+                  alt=""
+                />
+              </Link>
+              <div className={styles.content}>
+                <div className={styles.desc_box}>
+                  <div className={styles.row_content}>
+                    <div className={styles.col_content}>
+                      <h2>Hành trình tham quan</h2>
+                    </div>
+                    <div className={styles.col_contents}>
+                      <p>điểm đến phồn hoa</p>
+                      <p>Sinh thái giao hoà</p>
+                    </div>
+                  </div>
+                </div>
+                <span>khám phá ngay</span>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.item}>
+              <Link href="/">
+                <img
+                  src="https://aquacity.com.vn/wp-content/uploads/2021/04/dao-phuong-hoang-thumbnail.jpg"
+                  alt=""
+                />
+              </Link>
+              <div className={styles.content}>
+                <div className={styles.desc_box}>
+                  <div className={styles.row_content}>
+                    <div className={styles.col_content}>
+                      <h2>Hành trình tham quan</h2>
+                    </div>
+                    <div className={styles.col_contents}>
+                      <p>điểm đến phồn hoa</p>
+                      <p>Sinh thái giao hoà</p>
+                    </div>
+                  </div>
+                </div>
+                <span>khám phá ngay</span>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+}
