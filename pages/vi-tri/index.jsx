7b9d1282c330fd17b2fe248e299c22a85be97499
quanghuyTiPiCi <quanghuy.tipici@gmail.com>
2022-03-28 13:02:52 +0700

    upcode

--- pages/vi-tri/index.jsx
+++ pages/vi-tri/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './styles.module.scss   ';
+import styles from './styles.module.scss';
 import { CardMedia } from '@material-ui/core';
 
 export default function Position() {
