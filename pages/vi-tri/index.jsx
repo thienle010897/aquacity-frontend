c79a8383c43e2c645bf507cae7b4afc55b21607b
thien <thien.tipici@gmail.com>
2022-03-29 09:08:12 +0700

    up code ecological

--- pages/vi-tri/index.jsx
+++ pages/vi-tri/index.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import Vitri from "../../public/image/vi-tri.jpg"
+import Image from 'next/image'
 
 export default function Position() {
   return (
-    <>
-      <div className={styles.video}>
-        <video autoPlay loop muted playsInline className={styles.ofcv}>
-          <source
-            src="https://aquacity.com.vn/wp-content/uploads/2021/05/banner.mp4"
-            type="video/mp4"
-            width="100%"
-          />
-        </video>
+      <div className={styles.image}>
+        <img
+          src="https://aquacity.com.vn/wp-content/uploads/2021/05/vitri_thumbnail_2-1-scaled.jpg"
+          alt="Picture of the author"
+        />
       </div>
-    </>
   );
 }
